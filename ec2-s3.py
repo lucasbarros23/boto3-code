4268198476c0cedffa9eb32155573a665b9da3a0
lucasbarros23 <70347936+lucasbarros23@users.noreply.github.com>
2021-01-27 13:49:58 -0300

    Update ec2-s3.py

--- ec2-s3.py
+++ ec2-s3.py
@@ -2,14 +2,14 @@ import boto3
 
 
 
- # criar uma nova instância EC2 instances
+ # Provisionar EC2
 ec2 = boto3.resource ( 'ec2' )
 ec2.create_instances ( ImageId = 'ami-0885b1f6bd170450c' , MinCount = 1, MaxCount = 2, InstanceType = 't2.micro')
 
 
-
+#Provisionar S3
 s3 = boto3.client('s3')
-s3.create_bucket(Bucket='my-bucket-dos-deuses-alado')
+s3.create_bucket(Bucket='NOME-DO-SEU-BUCKETT')
 
 
 
